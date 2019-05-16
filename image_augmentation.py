92f8a7f5d9c76a18a48be6bfbc1b05d2e51322a4
DrJessop <14ag89@queensu.ca>
2019-05-16 11:55:51 -0400

    Added an extra translational function to translate images in x,y,z directions

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,6 +1,8 @@
 import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import pprint as pp
 
 
 def resample(image, transform):
@@ -30,18 +32,60 @@ def get_center(img):
 
 
 def rotation3d(image, theta_x, theta_y, theta_z, show=False):
-    # theta_x, theta_y, theta_z = deg_to_rad()
-    theta_x = np.pi * theta_x / 180
-    theta_y = np.pi * theta_y / 180
-    theta_z = np.pi * theta_z / 180
-    euler_transform = sitk.Euler3DTransform()
+    """
+    This function rotates an image across each of the x, y, z axes by theta_x, theta_y, and theta_z degrees
+    respectively
+    :param image: An sitk MRI image
+    :param theta_x: The amount of degrees the user wants the image rotated around the x axis
+    :param theta_y: The amount of degrees the user wants the image rotated around the y axis
+    :param theta_z: The amount of degrees the user wants the image rotated around the z axis
+    :param show: Boolean, whether or not the user wants to see the result of the rotation
+    :return: The rotated image
+    """
+    theta_x = np.deg2rad(theta_x)
+    theta_y = np.deg2rad(theta_y)
+    theta_z = np.deg2rad(theta_z)
+    euler_transform = sitk.Euler3DTransform(get_center(image), theta_x, theta_y, theta_z, (0, 0, 0))
     image_center = get_center(image)
     euler_transform.SetCenter(image_center)
-    # print(euler_transform.GetCenter())
     euler_transform.SetRotation(theta_x, theta_y, theta_z)
-    # euler_transform.SetTranslation((0, 2.5, 0))
     resampled_image = resample(image, euler_transform)
     if show:
         plt.imshow(sitk.GetArrayFromImage(resampled_image)[0])
         plt.show()
     return resampled_image
+
+
+def translation3d(image, trans_x, trans_y, trans_z, show=False):
+    """
+    This function rotates an image across each of the x,y, z axes by trans_x, trans_y, and trans_x mm
+    respectively
+    :param image: An sitk MRI image
+    :param trans_x: The amount of mm the user wants the image translated by x mm
+    :param trans_y: The amount of mm the user wants the image translated by y mm
+    :param trans_z: The amount of mm the user wants the image translated by z mm
+    :param show: Boolean, whether or not the user wants to see the result of the translation
+    :return: The translated image
+    """
+    euler_transform = sitk.Euler3DTransform()
+    image_center = get_center(image)
+    euler_transform.SetTranslation((trans_x, trans_y, trans_z))
+    resampled_image = resample(image, euler_transform)
+    if show:
+        plt.imshow(sitk.GetArrayFromImage(resampled_image)[0])
+        plt.show()
+    return resampled_image
+
+
+if __name__ == "__main__":
+    img = sitk.ReadImage("/home/andrewg/PycharmProjects/assignments/resampled/t2/ProstateX-0000.nrrd")
+    img_arr = sitk.GetArrayFromImage(img)[0]
+    plt.imshow(img_arr)
+    plt.show()
+
+    input("Press enter to continue...")
+
+    # rotation3d(img, 0, 0, 45, show=True)
+    translation3d(img, 50, 50, 0, show=True)
+
+    pp.pprint(sorted(os.listdir("/home/andrewg/PycharmProjects/assignments/resampled/t2")))
